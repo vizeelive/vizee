42d0d38703957ccf0f1f34d852f64a4ed7d57397
Doug Richar <drichar@gmail.com>
2020-10-04 04:17:54 -0400

    Install Storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Theme from '../src/Theme'
+
+export const decorators = [(Story) => <Theme><Story/></Theme>]
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
