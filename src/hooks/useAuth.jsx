1f3e910e6f1bc78f406b98ebc8b707881c649816
Jeff Loiselle <jeff@loiselles.com>
2020-10-11 17:18:10 -0500

    removes linting errors

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import config from "../config";
-import { useEffect, useState, useMemo } from "react";
 
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
