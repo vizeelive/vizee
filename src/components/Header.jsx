2703e4d4f7f67fdb7c7ee5ebe07b92921ba163c6
Doug Richar <drichar@gmail.com>
2020-10-16 02:17:37 -0400

    Responsive text logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,28 +6,34 @@ import styled from 'styled-components';
 
 import Logo from '../components/Logo';
 import MainMenu from '../components/MainMenu';
+import useBreakpoint from '../hooks/useBreakpoint';
 
 const StyledHeader = styled(Layout.Header)`
   padding-left: 0;
+`;
 
-  .logo {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    height: 64px;
-    width: calc(2rem + 40px);
-    float: left;
-  }
+const LogoLink = styled(Link)`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 64px;
+  padding: 0 20px;
+  float: left;
 `;
 
 function Header(props) {
   const { user, account, onLogin, onLogout } = props;
+  const isLargeScreen = useBreakpoint( 'lg' );
 
   return (
     <StyledHeader>
-      <Link to="/" className="logo">
-        <Logo size={2} />
-      </Link>
+      <LogoLink to="/">
+        <Logo
+          size={isLargeScreen ? 7.5 : 2.29}
+          textColor="#fafafa"
+          hasText={isLargeScreen}
+        />
+      </LogoLink>
       <MainMenu
         user={user}
         account={account}
