20052443eb0e5ddbd7355508ec2a256cf28fa4c0
Doug Richar <drichar@gmail.com>
2020-10-15 23:40:17 -0400

    Update AddAccount

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Typography } from 'antd';
 
 import AddAccount from '../pages/AddAccount';
 
@@ -8,10 +9,14 @@ const Content = styled.div`
   min-height: calc(100vh - 64px);
 `;
 
+const CreateMessage = styled(Typography.Title)`
+  font-weight: 500 !important;
+`;
+
 export default function CreateAccount() {
   return (
     <Content>
-      <h1>Let's create an account.</h1>
+      <CreateMessage level={3}>Let's create an account.</CreateMessage>
       <AddAccount redirect="account" />
     </Content>
   );
