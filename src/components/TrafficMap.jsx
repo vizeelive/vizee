401d8eab478ee12f3a18ef231e4a64546ce14832
Jeff Loiselle <jeff@loiselles.com>
2020-10-20 08:56:37 -0500

    traffic maps zoom to layer bounds

--- src/components/TrafficMap.jsx
+++ src/components/TrafficMap.jsx
@@ -81,6 +81,11 @@ const Map = (props) => {
         }
       });
 
+      if (data.length > 1) {
+        let coords = data?.map((d) => d.geometry.coordinates);
+        map.current.fitBounds(coords, { padding: 50 });
+      }
+
       function animateMarker(timestamp) {
         timeout.current = setTimeout(function () {
           requestAnimationFrame(animateMarker);
