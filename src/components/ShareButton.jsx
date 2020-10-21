d595caa0312554579ebe16c7aaaf79d820c5aef0
Jeff Loiselle <jeff@loiselles.com>
2020-10-21 15:08:54 -0500

    fixes share button for account manage

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -47,6 +47,8 @@ export default function ShareButton() {
     message.success('Copied link');
   };
 
+  let url = window.location.href.replace('/manage/events', '');
+
   return (
     <React.Fragment>
       <Modal
@@ -55,16 +57,16 @@ export default function ShareButton() {
         footer={null}
         onCancel={() => setShareModalVisible(false)}
       >
-        <FacebookShareButton url={window.location.href}>
+        <FacebookShareButton url={url}>
           <FacebookIcon />
         </FacebookShareButton>
-        <TwitterShareButton url={window.location.href}>
+        <TwitterShareButton url={url}>
           <TwitterIcon />
         </TwitterShareButton>
-        <EmailShareButton url={window.location.href}>
+        <EmailShareButton url={url}>
           <EmailIcon />
         </EmailShareButton>
-        <CopyToClipboard text={window.location.href} onCopy={handleCopy}>
+        <CopyToClipboard text={url} onCopy={handleCopy}>
           <CopyButton className="react-share__ShareButton">
             <svg viewBox="0 0 24 24">
               <path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z" />
