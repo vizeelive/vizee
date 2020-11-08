e39cd98ab2efaf2d43bcb54de00de97bc1cac9e4
Jeff Loiselle <jeff@loiselles.com>
2020-11-08 16:31:32 -0600

    adds sentry (#65)
    Fixes VIZ-22

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn:
+    'https://16f3e02884104cff9010e2a196d9183e@o473703.ingest.sentry.io/5508932',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+});
 
 ReactDOM.render(
   <Auth0Provider
