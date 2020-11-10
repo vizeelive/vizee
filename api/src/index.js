b33a0d875de47e6fb536db7480ee25ab20c84513
Jeff Loiselle <jeff@loiselles.com>
2020-11-10 13:57:41 -0600

    adds sentry to api

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const app = require('./app');
 const bodyParser = require('body-parser');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
+
+Sentry.init({
+  dsn:
+    'https://91a739d55cb0416086666bd8c11a234c@o473703.ingest.sentry.io/5511747',
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+});
 
 require('./controllers/stripe');
 require('./controllers/mux');
