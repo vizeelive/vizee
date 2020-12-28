2fd7be6a903aaee5f7aca6d939e40a5117563751
Jeff Loiselle <jeff@loiselles.com>
2020-12-28 19:16:54 +0000

    send api logs to console

--- api/src/logger.js
+++ api/src/logger.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: 'vizee-api' },
   transports: [
     //
     // - Write all logs with level `error` and below to `error.log`
@@ -18,12 +18,12 @@ const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple()
-    })
-  );
-}
+// if (process.env.NODE_ENV !== 'production') {
+logger.add(
+  new winston.transports.Console({
+    format: winston.format.simple()
+  })
+);
+// }
 
 module.exports = logger;
