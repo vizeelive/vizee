cfd506a3599bae1f6583dadb53318411871bf1a3
Jeff Loiselle <jeff@loiselles.com>
2020-12-31 08:39:16 -0600

    fix migration

--- hasura/migrations/1609370139955_alter_table_public_users_add_column_code/up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "public"."users" ADD COLUMN "code" text NULL;
-ALTER TABLE ONLY "public"."users" ALTER COLUMN "code" SET DEFAULT 'random_string(7)'
-ALTER TABLE "public"."users" ADD CONSTRAINT "users_code_key" UNIQUE ("code");
