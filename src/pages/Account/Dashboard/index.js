a0241ea807d20331a5f2ac9ce7cbad306a834198
Jeff Loiselle <jeff@loiselles.com>
2021-02-23 17:03:58 -0600

    adds tips to dashboard
    Fixes VIZ-160

--- src/pages/Account/Dashboard/index.js
+++ src/pages/Account/Dashboard/index.js
@@ -11,6 +11,7 @@ const ACCOUNT_REPORT = gql`
       id
       username
       name
+      tips
       revenue
       favoritecount
       eventcount
--- src/pages/Account/Dashboard/view.jsx
+++ src/pages/Account/Dashboard/view.jsx
@@ -97,6 +97,30 @@ export default function DashboardView(props) {
               />
 
               <Statistic
+                name="Tips"
+                value={account.tips}
+                icon={
+                  <React.Fragment>
+                    {/* Heroicon name: currency-dollar */}
+                    <svg
+                      className="h-8 w-8 text-gray-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.5}
+                        d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </React.Fragment>
+                }
+              />
+
+              <Statistic
                 name="Subscribers"
                 value={account.subscriptionscount}
                 icon={
