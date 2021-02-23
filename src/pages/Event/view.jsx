f21041de23d39247679a9f149d1ddee7e352a981
Jeff Loiselle <jeff@loiselles.com>
2021-02-23 11:29:07 -0600

    dont show unavailable date

--- src/pages/Event/view.jsx
+++ src/pages/Event/view.jsx
@@ -194,25 +194,6 @@ export default function EventPage(props) {
             </svg>
             {`Available on ${moment(event.start).format('MMMM Do h:mma')}`}
           </div>
-          <div className="mt-2 flex items-center text-sm text-gray-300 lg:mr-6">
-            {/* Heroicon name: calendar */}
-            <svg
-              className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-500"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            {`Until ${moment(event.end).format('MMMM Do h:mma')}`}
-          </div>
           {event.location && (
             <div
               className="mt-2 flex items-center text-sm text-gray-300 lg:mr-6"
