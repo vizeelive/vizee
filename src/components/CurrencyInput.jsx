267afd581d011189cc7b0e39c7783737a10e0eb3
Jeff Loiselle <jeff@loiselles.com>
2021-05-29 11:13:55 -0500

    removes comma from currency
    Fixes VIZ-260

--- src/components/CurrencyInput.jsx
+++ src/components/CurrencyInput.jsx
@@ -7,7 +7,7 @@ const defaultMaskOptions = {
   prefix: '$',
   suffix: '',
   includeThousandsSeparator: true,
-  thousandsSeparatorSymbol: ',',
+  thousandsSeparatorSymbol: '',
   allowDecimal: true,
   decimalSymbol: '.',
   decimalLimit: 2, // how many digits allowed after the decimal
