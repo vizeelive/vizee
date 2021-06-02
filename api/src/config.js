399982b2c3034dffa1b1d076a9557f74955d695e
Jeff Loiselle <jeff@loiselles.com>
2021-06-02 06:49:00 -0500

    updates api config

--- api/src/config.js
+++ api/src/config.js
@@ -1,14 +1,15 @@
 let config = {
   dev: {
-    api: 'https://staging-graphql.vizee.live/v1/graphql',
+    api: 'https://hasura-staging.vizee.live/v1/graphql',
+    // api: 'http://localhost:8080/v1/graphql',
     ui: 'https://dev.vizee.live:3000'
   },
   test: {
-    api: 'https://staging-graphql.vizee.live/v1/graphql',
+    api: 'https://hasura-staging.vizee.live/v1/graphql',
     ui: 'https://localhost:3000'
   },
   staging: {
-    api: 'https://staging-graphql.vizee.live/v1/graphql',
+    api: 'https://hasura-staging.vizee.live/v1/graphql',
     ui: 'https://staging.vizee.live'
   },
   production: {
@@ -18,5 +19,6 @@ let config = {
 };
 
 let env = process.env.NODE_ENV || 'dev';
+console.log(env);
 
 module.exports = config[env];
