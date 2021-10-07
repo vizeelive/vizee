5ab8171cfbe2a56732c2a1c8fcd164f6dfc7286d
Jeff Loiselle <jeff@loiselles.com>
2021-10-07 16:14:41 -0500

    removes supporters column for disabled stripe accounts

--- src/pages/Account/Home/view.jsx
+++ src/pages/Account/Home/view.jsx
@@ -302,32 +302,32 @@ export default function HomeView(props) {
                 </TabPane> */}
               </Tabs>
             </div>
-            <div className="md:mt-11 rounded-lg md:ml-5 md:w-80 bg-gray-900 p-5">
-              <div className="text-center content-center">
-                <div className="mb-5">Can we please get your support? ❤️</div>
-                {account.stripe_data && (
+            {account.stripe_data && (
+              <div className="md:mt-11 rounded-lg md:ml-5 md:w-80 bg-gray-900 p-5">
+                <div className="text-center content-center">
+                  <div className="mb-5">Can we please get your support? ❤️</div>
                   <BuyButton isTip={true} user={user} account={account} />
-                )}
+                </div>
+                {account.supporters_report.map((user, index) => (
+                  <Card className="mt-3" key={index}>
+                    <img
+                      src={
+                        index <= 2
+                          ? `/icons/medal-${index + 1}.png`
+                          : `/icons/medal-4.png`
+                      }
+                      width="40"
+                      className="float-left mr-2"
+                    />
+                    <div className="text-l">
+                      {user.first_name && user.last_name
+                        ? `${user.first_name} ${user.last_name}`
+                        : 'Anonymous'}{' '}
+                    </div>
+                  </Card>
+                ))}
               </div>
-              {account.supporters_report.map((user, index) => (
-                <Card className="mt-3" key={index}>
-                  <img
-                    src={
-                      index <= 2
-                        ? `/icons/medal-${index + 1}.png`
-                        : `/icons/medal-4.png`
-                    }
-                    width="40"
-                    className="float-left mr-2"
-                  />
-                  <div className="text-l">
-                    {user.first_name && user.last_name
-                      ? `${user.first_name} ${user.last_name}`
-                      : 'Anonymous'}{' '}
-                  </div>
-                </Card>
-              ))}
-            </div>
+            )}
           </div>
           <br />
 
