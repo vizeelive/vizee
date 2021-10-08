1536702de1c10e3ebf779499337d878c4e2e037f
Jeff Loiselle <jeff@loiselles.com>
2021-10-08 02:41:59 -0500

    fixes master download when no video available

--- api/src/lib/getEventUrl.js
+++ api/src/lib/getEventUrl.js
@@ -4,7 +4,7 @@ const { getEvent, getUserAccess } = require('../queries');
 const { createToken, createPlaybackId } = require('../lib/mux');
 const { presignUrl, listBuckets } = require('../lib/aws');
 
-function parseUrl(url) {
+function parseUrl(url = '') {
   let pieces = url.replace('https://', '').split('/');
   let bucket = pieces[0].split('.')[0];
   pieces.shift();
