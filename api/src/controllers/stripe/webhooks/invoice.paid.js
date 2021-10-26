67992fdc187bab49dfece339dfbebd73cbcdaaf3
Jeff Loiselle <jeff@loiselles.com>
2021-10-26 06:41:05 -0500

    fixes invoice.paid webhook

--- api/src/controllers/stripe/webhooks/invoice.paid.js
+++ api/src/controllers/stripe/webhooks/invoice.paid.js
@@ -3,9 +3,9 @@ const dayjs = require('dayjs');
 const { updateSubscription } = require('../../../mutations');
 
 async function invoice_paid(event) {
-  const expiry = dayjs(event.data.object.period_end * 1000).format(
-    'YYYY-MM-DD HH:mm:ss'
-  );
+  const expiry = dayjs(
+    event.data.object.lines.data[0].period.end * 1000
+  ).format('YYYY-MM-DD HH:mm:ss');
   let subscription_id = event.data.object.subscription;
   await updateSubscription({ subscription_id, expiry });
 }
