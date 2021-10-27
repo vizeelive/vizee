be6003f3a865e0f5c4054695c66fe5a7bd3c204f
Jeff Loiselle <jeff@loiselles.com>
2021-10-27 17:22:51 -0500

    dash show stats when steps complete

--- src/pages/Account/Dashboard/view.jsx
+++ src/pages/Account/Dashboard/view.jsx
@@ -68,7 +68,7 @@ export default function DashboardView(props) {
             />
           )}
 
-          {!stepsComplete && (
+          {stepsComplete && (
             <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
               {/* Cards */}
 
