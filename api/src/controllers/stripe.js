576e78fd1220a619978ff8a2bdfa1dd02e33c10e
Jeff Loiselle <jeff@loiselles.com>
2021-11-05 06:20:36 -0500

    Revert "adds capabilities requests"
    This reverts commit bc4600ed0000bf54e0f34886ace138b42ce77f9a.

--- api/src/controllers/stripe.js
+++ api/src/controllers/stripe.js
@@ -31,14 +31,6 @@ app.get('/stripe/account/create', async function (req, res) {
     logger.info('Creating new stripe account');
     var account = await stripe.accounts.create({
       type: 'express',
-      capabilities: {
-        card_payments: {
-          requested: true
-        },
-        transfers: {
-          requested: true
-        }
-      },
       settings: {
         payouts: {
           schedule: {
