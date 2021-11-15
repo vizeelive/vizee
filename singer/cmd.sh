f7821a1e4efde8e7ce083d4e5cd3f5a6a92eb29a
Jeff Loiselle <jeff@loiselles.com>
2021-11-15 11:16:23 -0600

    switch singer from cron to loop

--- singer/cmd.sh
+++ singer/cmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-/usr/local/bin/tap-stripe -c /config/stripe-config.json --catalog /code/catalog.json --state /config/state.json | /usr/local/bin/target-postgres -c /config/postgres-config.json >>/config/state.json
-tail -1 /config/state.json >/config/state.json.tmp
-mv /config/state.json.tmp /config/state.json
+while true; do
+    /usr/local/bin/tap-stripe -c /config/stripe-config.json --catalog /code/catalog.json --state /config/state.json | /usr/local/bin/target-postgres -c /config/postgres-config.json >>/config/state.json
+    tail -1 /config/state.json >/config/state.json.tmp
+    mv /config/state.json.tmp /config/state.json
+    sleep 60
+done
