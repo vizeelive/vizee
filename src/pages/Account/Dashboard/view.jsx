a01371ca2160bb722b54e26c5f4846b15e7e1016
Jeff Loiselle <jeff@loiselles.com>
2021-11-15 15:41:50 -0600

    fixes dashboard

--- src/pages/Account/Dashboard/view.jsx
+++ src/pages/Account/Dashboard/view.jsx
@@ -90,7 +90,7 @@ export default function DashboardView(props) {
             />
           )}
 
-          {!stepsComplete && (
+          {stepsComplete && (
             <div>
               <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
                 {/* Cards */}
