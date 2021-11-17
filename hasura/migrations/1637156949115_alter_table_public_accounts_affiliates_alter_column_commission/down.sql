d2bdc044d219e882627e6d3bb85de459f4666719
Jeff Loiselle <jeff@loiselles.com>
2021-11-17 09:59:51 -0600

    adds subscriptions.email perm and accounts_affiliates

---
+++ hasura/migrations/1637156949115_alter_table_public_accounts_affiliates_alter_column_commission/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."accounts_affiliates" ALTER COLUMN "commission" SET NOT NULL;
---
+++ hasura/migrations/1637156949115_alter_table_public_accounts_affiliates_alter_column_commission/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."accounts_affiliates" ALTER COLUMN "commission" DROP NOT NULL;
