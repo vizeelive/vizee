32ea3bf308fd9ec0ed784ea477466cce8b32843e
Jeff Loiselle <jeff@loiselles.com>
2021-11-18 08:51:49 -0600

    adds email to logout menu

--- src/components/layout/ProfileMenu.js
+++ src/components/layout/ProfileMenu.js
@@ -164,7 +164,7 @@ export default function ProfileMenu(props) {
             role="menuitem"
             onClick={onLogout}
           >
-            Logout
+            Logout {user.email}
           </button>
         </div>
       </Transition>
