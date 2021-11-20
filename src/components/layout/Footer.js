8ec4bd4f6f4aa20d551c5cd63eae0e95369c1907
Jeff Loiselle <jeff@loiselles.com>
2021-11-20 11:25:40 -0600

    adds copyright range to footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ export default function Footer({ className }) {
     <footer className={className}>
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <p className="text-center text-base text-gray-500 m-0">
-          &copy; {`${new Date().getFullYear()} `}
+          &copy; 2020-{`${new Date().getFullYear()} `}
           <a
             href="https://vizee.live"
             className="text-primary hover:text-pink-700 transition-none"
