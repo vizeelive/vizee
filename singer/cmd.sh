fff5a6c4a89ccab797cabb5e5420d61aef539151
Jeff Loiselle <jeff@loiselles.com>
2022-03-19 12:42:06 +0000

    fixes singer command

--- singer/cmd.sh
+++ singer/cmd.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-set -xeuf -o pipefail
-
 if [ -z "$1" ]; then
-  $1 = 'null';
+  export CMD=null
+else
+  export CMD=$1
 fi
 
-if [ "$1" = "-single" ]; then
+set -xeuf -o pipefail
+
+if [ $CMD = "-single" ]; then
     /usr/local/bin/tap-stripe -c /config/stripe-config.json --catalog /code/catalog.json --state /config/state.json | /usr/local/bin/target-postgres -c /config/postgres-config.json >>/config/state.json
     tail -1 /config/state.json >/config/state.json.tmp
     mv /config/state.json.tmp /config/state.json
